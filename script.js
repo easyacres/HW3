5dd4706c2b7d4c0f1390f9f0fd160d328d38a568
FBB <flybigbusiness@gmail.com>
2021-06-01 23:47:24 -0400

    repo update

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 
 var input1 = prompt("Please input a password length between 8 and 128");
     alert ("You chose " + input1 + " for the length of your new password");
- 
+
+if (isNaN(length)===true){
+    alert("Password Length must be a number");
+}
+
+if (input1<6){
+    alert("Length must be Min of 68")
+}
+
 function generate() {
 
     let pwcv = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+";
